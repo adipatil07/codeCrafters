0686b5e582ce98bb68f2089cf3900b425883df54
dypiemr123 <152372492+dypiemr123@users.noreply.github.com>
2024-02-10 19:35:01 +0530

    new online database

--- admin/include/config.php
+++ admin/include/config.php
@@ -1,9 +1,9 @@
 <?PHP
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "db_hackthaon";
+$servername = "89.117.157.103";
+$username = "u119271849_user_hakathon";
+$password = "Admin@#54321";
+$dbname = "u119271849_db_hakathon";
 
 $con = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
