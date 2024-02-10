b3f516ede38f1a7c7c932e7dbc7f3dde7f84ddef
dypiemr123 <152372492+dypiemr123@users.noreply.github.com>
2024-02-11 02:00:34 +0530

    club re list

--- clubRegistration.php
+++ clubRegistration.php
@@ -146,10 +146,13 @@
         <div class="card card-lg mb-5">
           <div class="card-body">
             <!-- Form -->
-            <form class="js-validate needs-validation" novalidate>
+            <form action="#" method="post">
               <div class="text-center">
                 <div class="mb-5">
-                  <h1 class="display-5">Join our Club</h1>
+                  <h1 class="display-4">Join Club</h1>
+                  <h2 class="display-5">Club Name : <?php extract($_GET); echo $club_name; ?></h2>
+                  <h2 class="display-5">Club ID : <?php echo $club_id; ?></h2>
+
                   <!-- <p>Already have an account? <a class="link" href="./authentication-login-basic.html">Sign in here</a></p> -->
                 </div>
 
@@ -171,13 +174,13 @@
             
               <div class="mb-4">
                 <label class="form-label" for="signupSrEmail">Department</label>
-                <input type="email" class="form-control form-control-lg" name="email" id="signupSrEmail" placeholder="CS" aria-label="Markwilliams@site.com" required>
+                <input type="text" class="form-control form-control-lg" name="dept" id="signupSrEmail" placeholder="CS" aria-label="Markwilliams@site.com" required>
                 <span class="invalid-feedback">Please Enter your Department name</span>
               </div>
 
               <div class="mb-4">
                 <label class="form-label" for="departmentDropdown">Domain</label>
-                <select class="form-select form-select-lg" id="departmentDropdown" name="department" required>
+                <select class="form-select form-select-lg" id="departmentDropdown" name="domain" required>
                 <option value="" selected disabled>Choose Your Domain</option>
                 <option value="AD">Android Development</option>
                 <option value="WD">Web Development</option>
@@ -191,13 +194,13 @@
 
               <div class="mb-4">
                 <label class="form-label" for="signupSrEmail">LinkedIn Profile</label>
-                <input type="email" class="form-control form-control-lg" name="email" id="signupSrEmail" placeholder="https://www.linkedin.com/in/xyz" aria-label="Markwilliams@site.com" required>
+                <input type="text" class="form-control form-control-lg" name="linkdean" id="signupSrEmail" placeholder="https://www.linkedin.com/in/xyz" aria-label="Markwilliams@site.com" required>
                 <span class="invalid-feedback">Please Enter your LinkedIn Profile link</span>
               </div>
 
               <div class="mb-4">
                 <label class="form-label" for="signupSrEmail">Github Profile</label>
-                <input type="email" class="form-control form-control-lg" name="email" id="signupSrEmail" placeholder="https://github.com/xyz" aria-label="Markwilliams@site.com" required>
+                <input type="text" class="form-control form-control-lg" name="git_hub" id="signupSrEmail" placeholder="https://github.com/xyz" aria-label="Markwilliams@site.com" required>
                 <span class="invalid-feedback">Please enter your Github Profile link</span>
               </div>
 
@@ -254,7 +257,7 @@
               <!-- End Form Check -->
 
               <div class="d-grid gap-2">
-                <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+                <button type="submit" class="btn btn-primary btn-lg" name="btn_submit">Submit</button>
 
               </div>
             </form>
@@ -300,3 +303,29 @@
   </script>
 </body>
 </html>
+<?php
+if(isset($_POST['btn_submit'])){
+  print_r($_POST);
+  extract($_POST);
+include('admin/include/config.php');
+
+// Inserting values into the database
+$sql = "INSERT INTO `tbl_user_club_req` 
+        (`req_user_id`, `req_user_name`, `req_user_email`, `req_user_mobile`, `req_user_dept`, `req_user_domain`, `reg_user_linkedin`, `reg_user_github`, `req_user_msg`,`req_club_name`,`req_club_id`) 
+        VALUES 
+        (NULL, '$fullName', '$email', '', '$dept', '$domain', '$linkdean', '$git_hub', '$reasonForJoining','$club_name','$club_id')";
+
+if(mysqli_query($con, $sql)){
+    echo "<script>alert('Records inserted successfully.'); window.location.href = 'index.php';</script>";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+}
+
+
+
+}
+
+
+
+
+?>
